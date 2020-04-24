a434f556f52852e130b793a359a8f617e79265db
KL <dev.lau3@gmail.com>
2020-04-24 10:37:29 -0500

    convert unconfirmed sort component to function

--- bike-safety/src/components/unconfirmedSort.js
+++ bike-safety/src/components/unconfirmedSort.js
@@ -6,96 +6,80 @@ import GraphByZip from './GraphByZip';
 import ImageRow from './ImageRow';
 import genericImg from '../assets/zip.webp'
 
-export default class UnconfirmedSort extends Component {
-    constructor(props) {
-        super(props);
+const displayMostRecentUnconfirmed = (props) => {
 
-        this.state={
+    let reportsByType = props.location.getReportsByTypeCallback();
 
-            redirectToHome: false 
+    if ( reportsByType === JSON.stringify({}) ) {
+    return <div></div>      //no data to display
+    }
+
+    reportsByType = JSON.parse(reportsByType);
+
+    let unconfirmedReports=reportsByType["unconfirmed"]; 
+
+    //sort by occurred_at date
+    unconfirmedReports.sort(function(a, b) {
+        return b.occurred_at - a.occurred_at;
+    })
+
+    //unconfirmed reports do not come with images. use generic images 
+    let unconfirmedObjList=[];
+    for (let i=0; i<unconfirmedReports.length; i++) {
+
+        let description="";
+        if (unconfirmedReports[i].description === null ||
+            unconfirmedReports[i].description.length <= 0) {
+            continue;
+        }
+
+        description = unconfirmedReports[i].description;
+
+        //Take only first 30 words.
+        let maxLength=30;
+        let descArray = description.trim().split(" ").slice(0, maxLength);
+        description = descArray.join(' ');
+
+        unconfirmedObjList.push(  {bikeImg: genericImg,
+                    reportTitle: unconfirmedReports[i].title,
+                    description: description });
+
+        if ( unconfirmedObjList.length >= 3 ) {   //only need 3 reports
+            break;
         }
     }
+
+    return (
+    <div className="bike-img-row">
+
+        <ImageRow imgObjList={JSON.stringify(unconfirmedObjList)} />
+
+    </div>
+    )
+}
+
+export default function UnconfirmedSort (props) {
     
-    displayMostRecentUnconfirmed() {
+    if (props.location.getLocationsByTypeCallback === undefined) {
+        return <div></div>    //no callback to get data
+    }
 
-        let reportsByType = this.props.location.getReportsByTypeCallback();
+    let toContainerId="unconfirmed-sort-container";
+        
+    props.location.swapDisplayCallback(toContainerId, props);
 
-        if ( reportsByType === JSON.stringify({}) ) {
-        return <div></div>      //no data to display
-        }
+    return (
+        <div id={toContainerId}>
 
-        reportsByType = JSON.parse(reportsByType);
+        <GraphByZip  
+            reportType={'unconfirmed'} 
+            graphTitle={'Unconfirms by Zip codes'}
+            getLocationsByTypeCallback={props.location.getLocationsByTypeCallback} />
 
-        let unconfirmedReports=reportsByType["unconfirmed"]; 
-
-        //sort by occurred_at date
-        unconfirmedReports.sort(function(a, b) {
-            return b.occurred_at - a.occurred_at;
-        })
-
-        //unconfirmed reports do not come with images. use generic images 
-        let unconfirmedObjList=[];
-        for (let i=0; i<unconfirmedReports.length; i++) {
-
-            let description="";
-            if (unconfirmedReports[i].description === null ||
-                unconfirmedReports[i].description.length <= 0) {
-                continue;
-            }
-
-            description = unconfirmedReports[i].description;
-
-            //Take only first 30 words.
-            let maxLength=30;
-            let descArray = description.trim().split(" ").slice(0, maxLength);
-            description = descArray.join(' ');
-
-            unconfirmedObjList.push(  {bikeImg: genericImg,
-                        reportTitle: unconfirmedReports[i].title,
-                        description: description });
-
-            if ( unconfirmedObjList.length >= 3 ) {   //only need 3 reports
-                break;
-            }
-        }
-
-        return (
-        <div className="bike-img-row">
-
-            <ImageRow imgObjList={JSON.stringify(unconfirmedObjList)} />
+        {displayMostRecentUnconfirmed(props)}
 
         </div>
-        )
-    }
-        
-    render() {
+    )
 
-        if (this.props.location.getLocationsByTypeCallback === undefined) {
-            return <div></div>    //no callback to get data
-        }
-
-        let toContainerId="unconfirmed-sort-container";
-        if (! this.state.redirectToHome) {  //do not overwrite display setup by filter form if redirecting away 
-            
-            this.props.location.swapDisplayCallback(toContainerId, this.props);
-        }
-
-        return (
-            <div id={toContainerId}>
-                
-                {this.state.redirectToHome &&
-                        <Redirect to='/Home' />    //route back to root (App component) depending on state
-                }
-
-            <GraphByZip  
-                reportType={'unconfirmed'} 
-                graphTitle={'Unconfirms by Zip codes'}
-                getLocationsByTypeCallback={this.props.location.getLocationsByTypeCallback} />
-
-            {this.displayMostRecentUnconfirmed()}
-
-            </div>
-        )
-    }
 }
 
