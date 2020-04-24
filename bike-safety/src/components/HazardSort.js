62235f135246c81e2dfb8b4e0a575e5ccee81fd2
KL <dev.lau3@gmail.com>
2020-04-24 10:29:12 -0500

    convert hazard sort component to function

--- bike-safety/src/components/HazardSort.js
+++ bike-safety/src/components/HazardSort.js
@@ -6,96 +6,80 @@ import GraphByZip from './GraphByZip';
 import ImageRow from './ImageRow';
 import genericImg from '../assets/bike-trail.jpg'
 
-export default class HazardSort extends Component {
-    constructor(props) {
-        super(props);
+const displayMostRecentHazards = (props) => {
 
-        this.state={
+    let reportsByType = props.location.getReportsByTypeCallback();
 
-            redirectToHome: false 
+    if ( reportsByType === JSON.stringify({}) ) {
+    return <div></div>      //no data to display
+    }
+
+    reportsByType = JSON.parse(reportsByType);
+
+    let hazardReports=reportsByType["hazard"]; 
+
+    //sort by occurred_at date
+    hazardReports.sort(function(a, b) {
+        return b.occurred_at - a.occurred_at;
+    })
+
+    //hazard reports do not come with images. use generic images 
+    let hazardObjList=[];
+    for (let i=0; i<hazardReports.length; i++) {
+
+        let description="";
+        if (hazardReports[i].description === null  ||
+            hazardReports[i].description.length <= 0) {
+            continue
+        }
+
+        description = hazardReports[i].description;
+
+        //Take only first 30 words.
+        let maxLength=30;
+        let descArray = description.trim().split(" ").slice(0, maxLength);
+        description = descArray.join(' ');
+
+        hazardObjList.push(  {bikeImg: genericImg,
+                    reportTitle: hazardReports[i].title,
+                    description: description });
+
+        if ( hazardObjList.length >= 3 ) {   //only need 3 reports
+            break;
         }
     }
-    
-    displayMostRecentHazards() {
 
-        let reportsByType = this.props.location.getReportsByTypeCallback();
+    return (
+    <div className="bike-img-row">
 
-        if ( reportsByType === JSON.stringify({}) ) {
-        return <div></div>      //no data to display
-        }
+        <ImageRow imgObjList={JSON.stringify(hazardObjList)} />
 
-        reportsByType = JSON.parse(reportsByType);
+    </div>
+    )
+}
 
-        let hazardReports=reportsByType["hazard"]; 
+export default function HazardSort (props)  {
 
-        //sort by occurred_at date
-        hazardReports.sort(function(a, b) {
-            return b.occurred_at - a.occurred_at;
-        })
+    if (props.location.getLocationsByTypeCallback === undefined) {
+        return <div></div>    //no callback to get data
+    }
 
-        //hazard reports do not come with images. use generic images 
-        let hazardObjList=[];
-        for (let i=0; i<hazardReports.length; i++) {
+    let toContainerId="hazard-sort-container";
+        
+        props.location.swapDisplayCallback(toContainerId, props);
 
-            let description="";
-            if (hazardReports[i].description === null  ||
-                hazardReports[i].description.length <= 0) {
-                continue
-            }
+    return (
+        <div id={toContainerId}>
 
-            description = hazardReports[i].description;
+        <GraphByZip  
+            reportType={'hazard'} 
+            graphTitle={'Hazards by Zip codes'}
+            getLocationsByTypeCallback={props.location.getLocationsByTypeCallback} />
 
-            //Take only first 30 words.
-            let maxLength=30;
-            let descArray = description.trim().split(" ").slice(0, maxLength);
-            description = descArray.join(' ');
-
-            hazardObjList.push(  {bikeImg: genericImg,
-                        reportTitle: hazardReports[i].title,
-                        description: description });
-
-            if ( hazardObjList.length >= 3 ) {   //only need 3 reports
-                break;
-            }
-        }
-
-        return (
-        <div className="bike-img-row">
-
-            <ImageRow imgObjList={JSON.stringify(hazardObjList)} />
+        {displayMostRecentHazards(props)}
 
         </div>
-        )
-    }
-        
-    render() {
+    )
 
-        if (this.props.location.getLocationsByTypeCallback === undefined) {
-            return <div></div>    //no callback to get data
-        }
-
-        let toContainerId="hazard-sort-container";
-        if (! this.state.redirectToHome) {  //do not overwrite display setup by filter form if redirecting away 
-            
-            this.props.location.swapDisplayCallback(toContainerId, this.props);
-        }
-
-        return (
-            <div id={toContainerId}>
-                
-                {this.state.redirectToHome &&
-                        <Redirect to='/Home' />    //route back to root (App component) depending on state
-                }
-
-            <GraphByZip  
-                reportType={'hazard'} 
-                graphTitle={'Hazards by Zip codes'}
-                getLocationsByTypeCallback={this.props.location.getLocationsByTypeCallback} />
-
-            {this.displayMostRecentHazards()}
-
-            </div>
-        )
-    }
 }
 
